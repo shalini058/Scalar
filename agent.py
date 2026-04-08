4ea2b8e2d5aed4a7111626441d1813f4ebe70bf5
shalini058 <132085825+shalini058@users.noreply.github.com>
2026-04-08 23:01:42 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,15 +1,9 @@
 import random
 
 class Agent:
-    def choose_action(self, state):
+    def choose_action(self, routes):
         best_action = 0
         best_score = float('inf')
-
-        routes = [
-            {"time": 5, "traffic": 2},
-            {"time": 8, "traffic": 1},
-            {"time": 6, "traffic": 3}
-        ]
 
         for i, r in enumerate(routes):
             score = r["time"] + r["traffic"]
